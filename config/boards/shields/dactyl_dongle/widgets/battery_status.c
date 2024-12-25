e49d964bd62bf2c9eb8f264bea633c4b799e02f1
Putitii <aadbaptista@gmail.com>
2024-12-25 21:16:14 +0000

    modified:   config/boards/shields/dactyl_dongle/CMakeLists.txt 	modified:   config/boards/shields/dactyl_dongle/Kconfig.defconfig 	modified:   config/boards/shields/dactyl_dongle/custom_status_screen.c 	modified:   config/boards/shields/dactyl_dongle/widgets/battery_status.c 	new file:   config/boards/shields/dactyl_dongle/widgets/bongo_cat.c 	new file:   config/boards/shields/dactyl_dongle/widgets/bongo_cat.h 	new file:   config/boards/shields/dactyl_dongle/widgets/bongo_cat_images.c 	deleted:    config/boards/shields/dactyl_dongle/widgets/hid_indicators.c 	deleted:    config/boards/shields/dactyl_dongle/widgets/hid_indicators.h 	new file:   config/boards/shields/dactyl_dongle/widgets/layer_status.c 	new file:   config/boards/shields/dactyl_dongle/widgets/layer_status.h 	new file:   config/boards/shields/dactyl_dongle/widgets/modifiers.c 	new file:   config/boards/shields/dactyl_dongle/widgets/modifiers.h 	new file:   config/boards/shields/dactyl_dongle/widgets/modifiers_sym.c 	modified:   config/boards/shields/dactyl_dongle/widgets/output_status.c

--- config/boards/shields/dactyl_dongle/widgets/battery_status.c
+++ config/boards/shields/dactyl_dongle/widgets/battery_status.c
@@ -22,68 +22,43 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-struct peripheral_battery_state {
-    uint8_t source;
-    uint8_t level;
+struct battery_status_state {
+    uint8_t level[ZMK_SPLIT_BLE_PERIPHERAL_COUNT];
+} battery_state;
+
+struct battery_widget_object {
+    lv_obj_t *battery_label;
 };
-    
-static lv_color_t battery_image_buffer[ZMK_SPLIT_BLE_PERIPHERAL_COUNT][5 * 8];
 
-static void draw_battery(lv_obj_t *canvas, uint8_t level) {
-    lv_canvas_fill_bg(canvas, lv_color_black(), LV_OPA_COVER);
-    
-    lv_draw_rect_dsc_t rect_fill_dsc;
-    lv_draw_rect_dsc_init(&rect_fill_dsc);
-    rect_fill_dsc.bg_color = lv_color_white();
+struct battery_widget_object battery_widget_objects[ZMK_SPLIT_BLE_PERIPHERAL_COUNT];
 
-    lv_canvas_set_px(canvas, 0, 0, lv_color_white());
-    lv_canvas_set_px(canvas, 4, 0, lv_color_white());
+static void set_battery_symbol(lv_obj_t *widget, struct battery_status_state state) {
+    for (int i = ZMK_SPLIT_BLE_PERIPHERAL_COUNT - 1; i >= 0; i--) {  // Iterate backwards
+        uint8_t level = state.level[i];
+        lv_obj_t *label = battery_widget_objects[i].battery_label;
+        
+        char text[5] = {};
 
-    if (level > 90) {
-        // full
-    } else if (level > 70) {
-        lv_canvas_draw_rect(canvas, 1, 2, 3, 1, &rect_fill_dsc);
-    } else if (level > 50) {
-        lv_canvas_draw_rect(canvas, 1, 2, 3, 2, &rect_fill_dsc);
-    } else if (level > 30) {
-        lv_canvas_draw_rect(canvas, 1, 2, 3, 3, &rect_fill_dsc);
-    } else if (level > 10) {
-        lv_canvas_draw_rect(canvas, 1, 2, 3, 4, &rect_fill_dsc);
-    } else {
-        lv_canvas_draw_rect(canvas, 1, 2, 3, 5, &rect_fill_dsc);
+        if (level > 0) {
+            snprintf(text, sizeof(text), "%3u%%", level);
+        }
+
+        lv_label_set_text(label, text);
     }
 }
 
-static void set_battery_symbol(lv_obj_t *widget, struct peripheral_battery_state state) {
-    lv_obj_t *symbol = lv_obj_get_child(widget, state.source * 2);
-    lv_obj_t *label = lv_obj_get_child(widget, state.source * 2 + 1);
-
-    draw_battery(symbol, state.level);
-    lv_label_set_text_fmt(label, "%3u%%", state.level);
-    
-    if (state.level > 0) {
-        lv_obj_clear_flag(symbol, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_clear_flag(label, LV_OBJ_FLAG_HIDDEN);
-    } else {
-        lv_obj_add_flag(symbol, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(label, LV_OBJ_FLAG_HIDDEN);
-    }
-}
-
-void battery_status_update_cb(struct peripheral_battery_state state) {
+void battery_status_update_cb(struct battery_status_state state) {
     struct zmk_widget_battery_status *widget;
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_battery_symbol(widget->obj, state); }
 }
 
-static struct peripheral_battery_state battery_status_get_state(const zmk_event_t *eh) {
+static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
     const struct zmk_peripheral_battery_state_changed *ev = as_zmk_peripheral_battery_state_changed(eh);
-    return (struct peripheral_battery_state){
-        .source = ev->source,
-        .level = ev->state_of_charge,
-    };
+    battery_state.level[ev->source] = ev->state_of_charge;
+    return battery_state;
 }
 
-ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct peripheral_battery_state,
+ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
                             battery_status_update_cb, battery_status_get_state)
 
 ZMK_SUBSCRIPTION(widget_battery_status, zmk_peripheral_battery_state_changed);
@@ -93,20 +68,18 @@ int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_batte
 
     lv_obj_set_size(widget->obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
 
-    for (int i = 0; i < ZMK_SPLIT_BLE_PERIPHERAL_COUNT; i++) {
-        lv_obj_t *image_canvas = lv_canvas_create(widget->obj);
-        lv_obj_t *battery_label = lv_label_create(widget->obj);
+    // Calculate initial x-offset for the rightmost label
+    int initial_x_offset = (ZMK_SPLIT_BLE_PERIPHERAL_COUNT - 1) * 35;
 
-        lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 5, 8, LV_IMG_CF_TRUE_COLOR);
+    for (int i = ZMK_SPLIT_BLE_PERIPHERAL_COUNT - 1; i >= 0; i--) {  // Iterate backwards
+        battery_widget_objects[i].battery_label = lv_label_create(widget->obj);
 
-        lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, 0, i * 10);
-        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -7, i * 10);
+        lv_obj_align(battery_widget_objects[i].battery_label, LV_ALIGN_LEFT_MID, initial_x_offset - i * 35, 0);
     }
 
     sys_slist_append(&widgets, &widget->node);
 
     widget_battery_status_init();
-
     return 0;
 }
 
