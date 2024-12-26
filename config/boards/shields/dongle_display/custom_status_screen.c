6fdc3964f9b5563c75cb0153fb97c77c93a2828e
Putitii <aadbaptista@gmail.com>
2024-12-26 14:38:48 +0000

    modified:   config/boards/shields/dongle_display/custom_status_screen.c

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -46,7 +46,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 
      zmk_widget_peripheral_battery_status_init(&peripheral_battery_status_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 20, 0);
+    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 10, 0);
 
     return screen;
 }
