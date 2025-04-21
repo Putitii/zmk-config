8211855c33f086b21220aaf3846f0f9a5061117e
Alexandre Baptista <alexandre.baptista@flymaster-avionics.com>
2025-04-21 15:07:19 +0100

    bb

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -50,7 +50,8 @@ lv_obj_t *zmk_display_status_screen() {
     // Battery Level Widget
     zmk_widget_peripheral_battery_status_init(&peripheral_battery_status_widget, screen);
     // lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 10, 0);
-    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    // lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
     return screen;
 }
